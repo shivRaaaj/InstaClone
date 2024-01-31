a0fcca1b89c977ee725040860a2d65b6b9d8a554
shivraj <dubeshivraj511@gmail.com>
2024-01-31 17:30:08 +0530

    Initial commit

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require('mongoose');
+const plm = require("passport-local-mongoose");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+mongoose.connect("mongodb://127.0.0.1:27017/instaclone");
+const userSchema = mongoose.Schema({
+  username: String,
+  password: String,
+  name: String,
+  email: String,
+  bio: String,
+  profileImage:String,
+  posts:[{ type: mongoose.Schema.Types.ObjectId, ref: "post" }],
 });
+userSchema.plugin(plm);
 
-module.exports = router;
+
+module.exports = mongoose.model("user", userSchema);
